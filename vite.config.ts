54a5938cf151d125b0817d87b6dc60860439983a
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2025-09-04 16:16:01 +0700

    fix(github pages): add base to resolve github.io repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/qrcodegen/',
 	plugins: [vue(), tailwindcss()],
 	optimizeDeps: {
 		force: true,
